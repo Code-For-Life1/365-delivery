deb27f2ab47810f07ea6f986c613122561ad0d3c
Vordead <mohamadhamade30@gmail.com>
2021-03-27 23:30:00 +0200

    added two drawer widgets, one for Merchant and one for Driver

---
+++ delivery_app/lib/customWidgets/merchantDrawer.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class MerchantDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          DrawerHeader(child: Text('Hello')),
+          ListTile(title: Text('A')),
+          ListTile(title: Text('B')),
+          SizedBox(height: 300),
+          TextButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed('/');
+              },
+              child: Text('Sign out', style: TextStyle(fontSize: 20))),
+        ],
+      ),
+    );
+  }
+}
