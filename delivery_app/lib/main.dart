6f075d0ef71b0f3d51c15cd6efa26bd2fedfba72
Vordead <mohamadhamade30@gmail.com>
2021-03-27 23:30:35 +0200

    modified main.dart to work with the route_generator.dart

--- delivery_app/lib/main.dart
+++ delivery_app/lib/main.dart
@@ -2,18 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:delivery_app/route_generator.dart';
 
-
-
-
 void main() => runApp(MaterialApp(
-    initialRoute: '/',
-    onGenerateRoute: RouteGenerator.generateRoute,
-    debugShowCheckedModeBanner: false,
-));
-//SystemChrome.setEnabledSystemUIOverlays([]);
+      initialRoute: '/',
+      onGenerateRoute: RouteGenerator.generateRoute,
+      debugShowCheckedModeBanner: false,
+    ));
 
-/*
-      home: MyApp(), debugShowCheckedModeBanner: false));
-  SystemChrome.setEnabledSystemUIOverlays([]);
-}
-*/
--- delivery_app/lib/route_generator.dart
+++ delivery_app/lib/route_generator.dart
@@ -1,17 +1,17 @@
-import 'package:delivery_app/driver_receiving_order.dart';
-import 'package:delivery_app/merchant_home_page.dart';
-import 'package:delivery_app/screens/driver_home_page.dart';
+import 'package:delivery_app/screens/driver/driver_pages/driver_receiving_order_page.dart';
+import 'package:delivery_app/screens/driver/driver_screens/driver_sign_in_screen.dart';
+import 'package:delivery_app/screens/driver/driver_screens/driver_sign_up_screen.dart';
+import 'package:delivery_app/screens/merchant/merchant_pages/merchant_drivers_page.dart';
+import 'package:delivery_app/screens/merchant/merchant_screens/merchant_home_screen.dart';
+import 'package:delivery_app/screens/merchant/merchant_screens/merchant_registers_driver_screen.dart';
+import 'package:delivery_app/screens/merchant/merchant_screens/merchant_sign_in_screen.dart';
+import 'package:delivery_app/screens/merchant/merchant_screens/merchant_sign_up_screen.dart';
+import 'screens/driver/driver_screens/driver_home_screen.dart';
 import 'package:delivery_app/test_screens/test3.dart';
 import 'test_screens/test1.dart';
 import 'test_screens/test2.dart';
 import 'package:flutter/material.dart';
 import 'package:delivery_app/screens/welcome.dart';
-import 'screens/driver_sign_up.dart';
-import 'screens/merchant_sign_up.dart';
-import 'screens/driver_sign_in.dart';
-import 'screens/merchant_sign_in.dart';
-
-
 
 class RouteGenerator {
   static Route<dynamic> generateRoute(RouteSettings settings) {
@@ -24,40 +24,48 @@ class RouteGenerator {
       case '/driverSignUpScreen':
         return MaterialPageRoute(builder: (_) => DriverSignUp());
       case '/merchantSignUpScreen':
-        return MaterialPageRoute(builder: (_)=>MerchantSignUp());
+        return MaterialPageRoute(builder: (_) => MerchantSignUp());
       case '/driverSignInScreen':
-        return MaterialPageRoute(builder: (_)=>DriverSignIn());
+        return MaterialPageRoute(builder: (_) => DriverSignIn());
       case '/merchantSignInScreen':
-        return MaterialPageRoute(builder: (_)=>MerchantSignIn());
+        return MaterialPageRoute(builder: (_) => MerchantSignIn());
       case '/driverHomePage':
-        return MaterialPageRoute(builder: (_)=>DriverHomePage());
+        return MaterialPageRoute(
+            builder: (_) => DriverHomeScreen(driverID: args));
       case '/merchantHomePage':
-        return MaterialPageRoute(builder: (_)=>MerchantHomePage(merchantID :args));
+        return MaterialPageRoute(
+            builder: (_) => MerchantHomeScreen(merchantID: args));
+      case '/merchantGetDrivers':
+        return MaterialPageRoute(
+            builder: (_) => MerchantDriversPage(merchantID: args));
       case '/driverReceivingOrder':
-        return MaterialPageRoute(builder: (_)=>DriverReceivingOrder());
+        return MaterialPageRoute(
+            builder: (_) => DriverReceivingOrder(driverID: args));
+      case '/driverAdd':
+        return MaterialPageRoute(
+            builder: (_) => MerchantRegisterDriver(merchantID: args));
       case '/test1':
-        return MaterialPageRoute(builder: (_)=>Test());
+        return MaterialPageRoute(builder: (_) => Test());
       case '/test2':
-        return MaterialPageRoute(builder: (_)=>Test2());
+        return MaterialPageRoute(builder: (_) => Test2());
       case '/test3':
-        return MaterialPageRoute(builder: (_)=>Test3());
+        return MaterialPageRoute(builder: (_) => Test3());
       default:
         //if there is no such named route in the switch statement, e.g /third
         return _errorRoute();
     }
   }
-  static Route<dynamic> _errorRoute(){
-    return MaterialPageRoute(
-        builder: (_){
-          return Scaffold(
-            appBar: AppBar(
-              title: Text("Error"),
-            ),
-            body: Center(
-              child: Text("Error"),
-            ),
-          );
-        }
-    );
+
+  static Route<dynamic> _errorRoute() {
+    return MaterialPageRoute(builder: (_) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text("Error"),
+        ),
+        body: Center(
+          child: Text("Error"),
+        ),
+      );
+    });
   }
 }
