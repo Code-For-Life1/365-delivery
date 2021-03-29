0e34134f2302b9d102f2dcabfda8c1a2c0133ac3
Vordead <mohamadhamade30@gmail.com>
2021-03-29 09:51:04 +0300

    added .idea to .gitignore

--- delivery_app/lib/main.dart
+++ delivery_app/lib/main.dart
@@ -2,10 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:delivery_app/route_generator.dart';
 
-
 void main() => runApp(MaterialApp(
       initialRoute: '/',
       onGenerateRoute: RouteGenerator.generateRoute,
       debugShowCheckedModeBanner: false,
     ));
-
