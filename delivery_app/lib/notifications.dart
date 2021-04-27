26c55665ef2c15b01aa50b5c1b2be252d0d10310
Vordead <mohamadhamade30@gmail.com>
2021-04-27 08:11:53 +0300

    updated notifications.dart to remove foreground notifications

--- delivery_app/lib/notifications.dart
+++ delivery_app/lib/notifications.dart
@@ -1,3 +1,4 @@
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
@@ -8,6 +9,7 @@ import 'models/notification_model.dart';
 import 'screens/driver/driver_screens/driver_home_screen.dart';
 import 'screens/merchant/merchant_screens/merchant_home_screen.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 String uniqueNotificationToken;
 
@@ -17,6 +19,15 @@ Map<String, String> getNotificationBody(String uniqueToken) {
     "type": "android",
   };
 }
+// FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+
+/// Create a [AndroidNotificationChannel] for heads up notifications
+// const AndroidNotificationChannel channel = AndroidNotificationChannel(
+//   'high_importance_channel', // id
+//   'High Importance Notifications', // title
+//   'This channel is used for important notifications.', // description
+//   importance: Importance.high,
+// );
 
 Future<void> notificationHandler() async {
   // Get the token each time the application loads
@@ -37,9 +48,54 @@ Future<void> notificationHandler() async {
       await FirebaseMessaging.instance.getInitialMessage();
   // Also handle any interaction when the app is in the background via a
   // Stream listener
-  FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {});
+  // FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+  //   // RemoteNotification notification = message.notification;
+  //   // AndroidNotification android = message.notification?.android;
+  //   // var androidDetails = AndroidNotificationDetails('channelId', 'channelName', 'channelDescription');
+  //   // var iosDetails = IOSNotificationDetails();
+  //   // var generalNotificationDetails = NotificationDetails(android: androidDetails,iOS: iosDetails);
+  //
+  //   // If `onMessage` is triggered with a notification, construct our own
+  //   // local notification to show to users using the created channel.
+  //   // if (notification != null && android != null) {
+  //   //   flutterLocalNotificationsPlugin.show(notification.hashCode, notification.title, notification.body, generalNotificationDetails);
+  //   //   flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+  //   //   var androidInit = AndroidInitializationSettings('@mipmap/ic_launcher');
+  //   //   var iosInit = IOSInitializationSettings();
+  //   //   var initSetting = InitializationSettings(android: androidInit,iOS: iosInit);
+  //   //   flutterLocalNotificationsPlugin.initialize(initSetting);
+  //   //
+  //   // } else
+  //   //   print('nothing');
+  //
+  // });
   FirebaseMessaging.onBackgroundMessage((message) => null);
+  //Foreground notification
 }
+
+// void foregroundNotify() {
+//   FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+//     RemoteNotification notification = message.notification;
+//     AndroidNotification android = message.notification?.android;
+//     var androidDetails = AndroidNotificationDetails('channelId', 'channelName', 'channelDescription');
+//     var iosDetails = IOSNotificationDetails();
+//     var generalNotificationDetails = NotificationDetails(android: androidDetails,iOS: iosDetails);
+//
+//     // If `onMessage` is triggered with a notification, construct our own
+//     // local notification to show to users using the created channel.
+//     if (notification != null && android != null) {
+//       FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+//       flutterLocalNotificationsPlugin.show(0, 'title', 'body', generalNotificationDetails);
+//       flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+//       var androidInit = AndroidInitializationSettings('@mipmap/ic_launcher');
+//       var iosInit = IOSInitializationSettings();
+//       var initSetting = InitializationSettings(android: androidInit,iOS: iosInit);
+//       flutterLocalNotificationsPlugin.initialize(initSetting);
+//
+//     } else
+//       print('nothing');
+//   });
+// }
 
 void deleteNotificationToken() {
   FirebaseMessaging.instance.deleteToken();
@@ -54,13 +110,17 @@ Future<void> saveTokenToDatabase(String token) async {
   var authenticationToken = await getAuthToken();
   authenticationToken == ""
       ? throw Exception("Authentication Token is null")
-      : http.post(Uri.parse('http://$ngrokLink/users/fcm/add_device'),
+      : http.post(Uri.parse('https://$httpLink/users/fcm/add_device'),
           body: jsonData,
           headers: {
               "content-type": "application/json",
               "Authorization": "Token $authenticationToken"
             });
 }
+
+
+
+
 
 Future<void> initializeDefault() async {
   FirebaseApp app = await Firebase.initializeApp();
