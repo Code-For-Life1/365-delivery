213a7f3648ad3db22486e14bbf0b08f49d8c07fb
Vordead <mohamadhamade30@gmail.com>
2021-04-29 03:30:51 +0300

    remove driver functionality

--- lib/screens/merchant/merchant_pages/merchant_drivers_page.dart
+++ lib/screens/merchant/merchant_pages/merchant_drivers_page.dart
@@ -20,6 +20,7 @@ class MerchantDriversPage extends StatefulWidget {
 }
 
 class _MerchantDriversPageState extends State<MerchantDriversPage> {
+  List<MerchantDrivers> driversDisplayed = [];
   Future<List<MerchantDrivers>> _getDrivers() async {
     var uri = Uri(
       scheme: 'https',
@@ -36,68 +37,15 @@ class _MerchantDriversPageState extends State<MerchantDriversPage> {
     var jsonData = json.decode(data.body);
     List<MerchantDrivers> drivers = [];
     for (var eachDriver in jsonData) {
-      MerchantDrivers driver =
-          MerchantDrivers(eachDriver["first_name"], eachDriver["last_name"], eachDriver["phone_number"]);
+      MerchantDrivers driver = MerchantDrivers(eachDriver["first_name"],
+          eachDriver["last_name"], eachDriver["phone_number"]);
       phoneNumber = eachDriver['phone_number'];
-      drivers.add(driver);
 
+      drivers.add(driver);
     }
     print(drivers.length);
+    driversDisplayed = drivers;
     return drivers;
-  }
-
-  void _showDialog() {
-    // flutter defined function
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        // return object of type Dialog
-        return AlertDialog(
-          title: new Text("Are you sure you want to remove this driver?"),
-          content: new Text("You will have to register the driver again"),
-          actions: <Widget>[
-            // usually buttons at the bottom of the dialog
-            new TextButton(
-              child: Row(
-                children: [
-                  TextButton(
-                      onPressed: ()async {
-                        var authenticationToken =
-                        await getAuthToken();
-                        http.delete(
-                            Uri.parse(
-                                'https://$httpLink//orders/merchant/delete_order/$phoneNumber'),
-                            headers: {
-                              "content-type":
-                              "application/json",
-                              "Authorization":
-                              "Token $authenticationToken"
-                            });
-                        setState(() {});
-                      },
-                      child: Text(
-                        'REMOVE DRIVER',
-                        style: TextStyle(color: Colors.red),
-                      )),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  TextButton(
-                      onPressed: () {Navigator.pop(context);},
-                      child: Text(
-                        'Cancel',
-                        style: TextStyle(fontSize: 16),
-                      ))
-                ],
-              ),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
-    );
   }
 
   @override
@@ -139,6 +87,76 @@ class _MerchantDriversPageState extends State<MerchantDriversPage> {
                 return ListView.builder(
                   itemCount: snapshot.data.length,
                   itemBuilder: (BuildContext context, int index) {
+                    String selectedDriverPhoneNumber =
+                        driversDisplayed[index].phoneNumber;
+                    void _showDialog() {
+                      // flutter defined function
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          // return object of type Dialog
+                          return AlertDialog(
+                            title: new Text(
+                                "Are you sure you want to remove this driver?"),
+                            content: new Text(
+                                "You will have to register the driver again"),
+                            actions: <Widget>[
+                              // usually buttons at the bottom of the dialog
+                              new TextButton(
+                                child: Row(
+                                  children: [
+                                    TextButton(
+                                        onPressed: () async {
+                                          Navigator.pop(context);
+
+                                          Map phoneNumberToBeDeleted = {
+                                            'phone_number':
+                                                selectedDriverPhoneNumber
+                                          };
+                                          var authenticationToken =
+                                              await getAuthToken();
+                                          var encodedPhoneNumber = json
+                                              .encode(phoneNumberToBeDeleted);
+                                          await http.delete(
+                                              Uri.parse(
+                                                  'https://$httpLink/users/merchant/delete_driver'),
+                                              body: encodedPhoneNumber,
+                                              headers: {
+                                                "content-type":
+                                                    "application/json",
+                                                "Authorization":
+                                                    "Token $authenticationToken"
+                                              });
+
+                                          setState(() {});
+                                        },
+                                        child: Text(
+                                          'REMOVE DRIVER',
+                                          style: TextStyle(color: Colors.red),
+                                        )),
+                                    SizedBox(
+                                      width: 10,
+                                    ),
+                                    TextButton(
+                                        onPressed: () {
+                                          Navigator.pop(context);
+                                        },
+                                        child: Text(
+                                          'Cancel',
+                                          style: TextStyle(fontSize: 16),
+                                        ))
+                                  ],
+                                ),
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    }
+
                     return Card(
                       color: Colors.orange,
                       shadowColor: Colors.blue,
